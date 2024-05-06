ee09ef211536763e14ed6fbe0506f195fdc1c130
Borrow228 <woolfegg@gmail.com>
2024-05-06 22:07:07 +0300

    Add hover effect

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ const container = document.querySelector("#container");
 
 let height = 16;
 let width = 16;
-
-container.style.maxWidth = width * 42 + "px";
+let squareWidth = 40;
+let squareHeight = 40;
+container.style.maxWidth = width * (squareWidth + 2) + "px";
 
 for (i = 0; i < (height * width); i ++) {
     let square = document.createElement("div");
     square.setAttribute("class", "square");
     square.textContent = "";
+    square.style.width = squareWidth + "px";
+    square.style.height = squareHeight + "px";
     container.appendChild(square);
+
+    square.addEventListener("mouseover", function () {
+        square.style.backgroundColor = "white";
+    })
 }
+
+
 
